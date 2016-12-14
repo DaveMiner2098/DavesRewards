09797319f92ebe598758bdf740eee6a56c3c4460
Dave Williams <Dave Williams>
2016-12-14 17:32:23 -0500

    initial add of sections

--- src/RBCRewardsRepository/LandingPage_Rep.java
+++ src/RBCRewardsRepository/LandingPage_Rep.java
@@ -5,12 +5,15 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 
 public class LandingPage_Rep
 {
+	@FindBy(how=How.CLASS_NAME, using = "contentframework-container-primary-custom")  
+	public WebElement WaysToRedeemYourPoints;
 	
 	
 	
